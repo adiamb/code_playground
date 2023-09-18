233982e67268e37f842908151715aa51f1548aa8
adiamb <ambati@Aditya-Ambati-MacBook-Pro.local>
2023-09-17 20:06:46 -0700

    run pre-commit hook

--- webapp/app.py
+++ webapp/app.py
@@ -5,35 +5,38 @@ from lib.query import search_pubmed
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/publications', methods=['GET'])
+
+@app.route("/publications", methods=["GET"])
 def get_publications():
-		author_name = 'aditya ambati'
+    author_name = "aditya ambati"
 
-		if author_name:
-				publications = search_pubmed(author_name)
-				
-				# Parse XML response from PubMed
-				parsed_publications = []
-				for xml_data in publications:
-						root = ET.fromstring(xml_data)
-						print(root)
-						try:
-							article = {
-									'title': root.find('.//ArticleTitle').text,
-									'journal': root.find('.//Title').text,
-									'year': root.find('.//PubDate/Year').text,
-									'abstract': root.find('.//AbstractText').text,
-							}
-						except AttributeError:
-							continue
-						parsed_publications.append(article)
-				return render_template('publications.html', publications=parsed_publications)
-		else:
-				return render_template('error.html', message='Author name not provided')
+    if author_name:
+        publications = search_pubmed(author_name)
 
-if __name__ == '__main__':
-		app.run(port=5000, threaded=True, host="0.0.0.0")
+        # Parse XML response from PubMed
+        parsed_publications = []
+        for xml_data in publications:
+            root = ET.fromstring(xml_data)
+            print(root)
+            try:
+                article = {
+                    "title": root.find(".//ArticleTitle").text,
+                    "journal": root.find(".//Title").text,
+                    "year": root.find(".//PubDate/Year").text,
+                    "abstract": root.find(".//AbstractText").text,
+                }
+            except AttributeError:
+                continue
+            parsed_publications.append(article)
+        return render_template("publications.html", publications=parsed_publications)
+    else:
+        return render_template("error.html", message="Author name not provided")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, threaded=True, host="0.0.0.0")
--- webapp/lib/query.py
+++ webapp/lib/query.py
@@ -4,25 +4,27 @@ import json
 
 def get_pubmed_api_key():
     try:
-        with open('creds.json', 'r') as file:
+        with open("creds.json", "r") as file:
             credentials = json.load(file)
-            return credentials.get('pubmed', None)
+            return credentials.get("pubmed", None)
     except FileNotFoundError:
         return None
 
+
 PUBMED_API = get_pubmed_api_key()
 
+
 def search_pubmed(author_name):
-    base_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-    search_url = base_url + 'esearch.fcgi'
-    fetch_url = base_url + 'efetch.fcgi'
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    search_url = base_url + "esearch.fcgi"
+    fetch_url = base_url + "efetch.fcgi"
 
     # Step 1: Perform a PubMed search to get the list of PubMed IDs for the author
     search_params = {
-        'db': 'pubmed',
-        'term': f'{author_name}[Author]',
-        'retmode': 'json',
-        'retmax':40
+        "db": "pubmed",
+        "term": f"{author_name}[Author]",
+        "retmode": "json",
+        "retmax": 40,
     }
     search_response = requests.get(search_url, params=search_params)
     search_data = search_response.json()
@@ -30,15 +32,15 @@ def search_pubmed(author_name):
     # Step 2: Fetch details for each publication using the PubMed IDs
     publications = []
 
-    if 'esearchresult' in search_data and 'idlist' in search_data['esearchresult']:
-        pubmed_ids = search_data['esearchresult']['idlist']
+    if "esearchresult" in search_data and "idlist" in search_data["esearchresult"]:
+        pubmed_ids = search_data["esearchresult"]["idlist"]
         for pubmed_id in pubmed_ids:
             fetch_params = {
-                'db': 'pubmed',
-                'id': pubmed_id,
-                'rettype': 'abstract',
-                'retmode': 'xml',
-                'api_key': PUBMED_API,
+                "db": "pubmed",
+                "id": pubmed_id,
+                "rettype": "abstract",
+                "retmode": "xml",
+                "api_key": PUBMED_API,
             }
             fetch_response = requests.get(fetch_url, params=fetch_params)
             publications.append(fetch_response.text)
